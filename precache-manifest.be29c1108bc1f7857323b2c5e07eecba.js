ed11abdea622565d4aa5360974382bbfe3ead63b
Slava <slava91petrushin@yandex.ru>
2020-07-01 21:13:32 +0300

    Updates

---
+++ precache-manifest.be29c1108bc1f7857323b2c5e07eecba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22220da7547ec19ad2975f83ee7ad9e8",
+    "url": "/skytrack_test/index.html"
+  },
+  {
+    "revision": "6656c009b0b47a9a4e08",
+    "url": "/skytrack_test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1ea20a277c8a2cc1a4ca",
+    "url": "/skytrack_test/static/js/2.17e81666.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/skytrack_test/static/js/2.17e81666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6656c009b0b47a9a4e08",
+    "url": "/skytrack_test/static/js/main.e45b233f.chunk.js"
+  },
+  {
+    "revision": "cc70db6fa9580193d766",
+    "url": "/skytrack_test/static/js/runtime-main.b06ae0fb.js"
+  }
+]);
